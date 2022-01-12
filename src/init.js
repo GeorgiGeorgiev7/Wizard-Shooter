e7323efaab5ae5f7f338c2bda28a8e92fda4c1eb
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-01-12 18:08:05 +0200

    Added wizard to play screen

--- src/init.js
+++ src/init.js
@@ -3,6 +3,9 @@ function gameFactory() {
 
     const startScreen = document.querySelector('.start-screen');
     const playScreen = document.querySelector('.play-screen');
+    const wizardElement = createWizard(100, 200);
+
+    playScreen.appendChild(wizardElement);
 
     Object.defineProperties(factory, {
         startScreen: {
@@ -11,6 +14,16 @@ function gameFactory() {
         playScreen: {
             get: () => playScreen
         },
+        wizardElement
     });
     return factory;
+}
+
+function createWizard(posX, posY) {
+    let wizardElement = document.createElement('div');
+    wizardElement.classList.add('wizard');
+    wizardElement.style.top = posY + 'px';
+    wizardElement.style.left = posX + 'px';
+
+    return wizardElement;
 }
