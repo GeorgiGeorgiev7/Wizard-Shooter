209438c323b7104aaff195c0bbb03dbaa5eb7093
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-01-12 22:34:53 +0200

    Added gravity to wizard

--- src/stateFactory.js
+++ src/stateFactory.js
@@ -6,7 +6,8 @@ function stateFactory() {
             y: 170,
             width:  82,
             height: 100,
-            speed: 4
+            speed: 4,
+            gravity: 2
         },
         bugStats: {
             nextBugCreation: 0,
